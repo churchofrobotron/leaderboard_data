6dafae2867c85854ff3ba9d423ec82c7c4f7c4cd
bzztbomb <skinny@knowhere.net>
2021-01-08 14:50:31 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Overview
+
+This is the data for the [Church of Robotron](https://churchofrobotron.com/) leaderboard. It is used for the [leaderboard](https://churchofrobotron.com/leaderboard/index.html) and [faces of futility](https://churchofrobotron.com/leaderboard/faces.html). 
+
+It is kept separate from the [main repo](https://github.com/churchofrobotron/mcor) to keep that repo size down.  
+
+# Leaderboard file info
+
+Each score is saved into the `data/` directory with a file name in this format:
+
+```
+[initials]_[score]_[datetime]_[location].gif
+```
+
+This collection of files is the "database" of scores for the church.  
+
+Then the `gen_score_db.js` script is run to generate JSON files that the website can use to display the leaderboard.
+
+# Wise words
+Try you hardest to get onto this leaderboard in the future. The last human family is counting on you, do not let them down. Check the [Church of Robotron](https://churchofrobotron.com/) for details on future installations.
+
---
+++ gen_score_db.js
@@ -0,0 +1,58 @@
+// 
+
+var fs = require("fs");
+
+files = fs.readdirSync("./data");
+
+var numGameSummary = {};
+var scores = [];
+
+for (i = 0; i < files.length; i++) {
+  file = files[i];
+  if (file.indexOf(".gif") != -1) {
+    var fields = file.split("_");
+    fields[1] = Number(fields[1]);
+    fields[2] = fields[2].replace(/T/g, " ");
+    fields[3] = fields[3].replace(/\.gif/g, "");
+    fields.splice(3, 0, file);
+    scores.push(fields);
+    if (numGameSummary[fields[4]] == null)
+        numGameSummary[fields[4]] = 0;
+    numGameSummary[fields[4]] = numGameSummary[fields[4]] + 1
+  }
+}
+
+scores.sort(function(a, b) {
+   var s1 = Number(a[1]);
+   var s2 = Number(b[1]);
+   if (s1>s2)
+       return -1;
+   if (s1<s2)
+       return 1;
+   return 0;
+});
+
+for (i = 0; i < scores.length; i++)
+  scores[i].push(i+1);
+
+let total = 0;
+for (i in numGameSummary) 
+  total += numGameSummary[i];
+numGameSummary["TOTAL"] = total;
+numGameSummary["DIODE_TOTAL"] = numGameSummary["DIODE"] + numGameSummary["WINDOW"] + numGameSummary["VR"];
+
+fs.writeFileSync("data/scoretable.js", 
+  `
+  var scoreTable = ${JSON.stringify(scores)};
+  var summaryTable = ${JSON.stringify(numGameSummary)};
+  `
+);
+
+fs.writeFileSync("data/summary.js",
+`
+var summaryTable = ${JSON.stringify(numGameSummary)};
+`
+)
+
+console.log('Complete.');
+
